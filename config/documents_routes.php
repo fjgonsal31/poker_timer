578fb593187b2c614d42b5dca15e1d4453c9cdf3
fjgonsal31 <fjgonsal31@gmail.com>
2024-09-25 14:25:35 +0200

    250924

---
+++ config/documents_routes.php
@@ -0,0 +1,12 @@
+<?php
+
+$root = "C:/xampp/htdocs/PHP/poker_timer";
+
+return [
+    'home' => $root . '/public/index.php',
+    'vHead' => $root . '/app/views/head.html',
+    'vMain' => $root . '/app/views/main.html',
+    'routes' => $root . '/config/routes.php',
+    'sMain' => $root . '/public/assets/css/main.css',
+    'routes' => $root . '/config/routes.php',
+];
---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+
+$documents_routes = require '../config/documents_routes.php';
+
+require_once $documents_routes['vHead'];
+require_once $documents_routes['vMain'];
