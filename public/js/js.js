5dedd13050f318b57171f76e79892048b987de88
fjgonsal31 <fjgonsal31@gmail.com>
2024-10-16 14:48:03 +0200

    161024

--- public/js/js.js
+++ public/js/js.js
@@ -1,5 +1,7 @@
 //------------ variables Main------------
 //***** botones *****
+// settings
+const settingsBtn = document.getElementById("settingsBtn");
 // level
 const downLevel = document.getElementById("lev-");
 const upLevel = document.getElementById("lev+");
@@ -58,15 +60,14 @@ const pPrizes = document.getElementById("prizes");
 
 //------------ variables Modal------------
 //***** botones *****
-const settingsBtn = document.getElementById("settingsBtn");
 const sendInfoBtnM = document.getElementById("infoDivSend");
 const resetInfoBtnM = document.getElementById("infoDivReset");
-const generatePrizes = document.getElementById("prizesGenerateBtn");
-const sendPrizesBtn = document.getElementById("prizesSendBtn");
+const generatePrizesBtnM = document.getElementById("prizesGenerateBtn");
+const sendPrizesBtnM = document.getElementById("prizesSendBtn");
 const closeM = document.getElementById("close");
 
 //***** elementos *****
-const inputs = document.querySelectorAll(".input");
+const inputs = document.querySelectorAll(".inputInfo");
 const modal = document.getElementById("myModal");
 const dateM = document.getElementById("dateModal");
 const nameM = document.getElementById("nameModal");
@@ -107,7 +108,7 @@ function updateCurrentTime() {
 // actualizar fila total premios
 function updateTotals() {
   let totalPercent = 0;
-  let totalPrize = 0;
+  let totalPrize = Number(document.getElementById("totalPrize").value);
 
   arrayPrizes.forEach((item) => {
     totalPercent += item.percent;
@@ -115,41 +116,24 @@ function updateTotals() {
   });
 
   totalRow = document.getElementById("totalRow");
-  if (!totalRow) {
-    totalRow = document.createElement("tr");
-    totalRow.id = "totalRow";
-    let tdTotalPlace = document.createElement("td");
-    tdTotalPlace.textContent = "Total";
-    totalRow.appendChild(tdTotalPlace);
 
-    let tdTotalPercent = document.createElement("td");
-    tdTotalPercent.textContent = totalPercent;
-    totalRow.appendChild(tdTotalPercent);
-
-    let tdTotalPrize = document.createElement("td");
-    tdTotalPrize.textContent = totalPrize;
-    totalRow.appendChild(tdTotalPrize);
-
-    tbodyPrizes.appendChild(totalRow);
-  } else {
-    totalRow.cells[1].textContent = totalPercent;
-    totalRow.cells[2].textContent = totalPrize;
-  }
+  totalRow.cells[1].textContent = totalPercent.toFixed(2);
+  totalRow.cells[2].textContent = totalPrize;
 
   if (totalRow.cells[1].textContent != 100) {
     totalRow.cells[0].style.color = "var(--red)";
     totalRow.cells[1].style.color = "var(--red)";
     totalRow.cells[2].style.color = "var(--red)";
-    sendPrizesBtn.disabled = true;
+    sendPrizesBtnM.disabled = true;
   } else {
     totalRow.cells[0].style.color = "var(--black)";
     totalRow.cells[1].style.color = "var(--black)";
     totalRow.cells[2].style.color = "var(--black)";
-    sendPrizesBtn.disabled = false;
+    sendPrizesBtnM.disabled = false;
   }
 }
 //------------ ejecucion ------------
-
+// //////////////////////////// MAIN ////////////////////////////
 // abrir modal settings
 settingsBtn.addEventListener("click", () => {
   modal.style.display = "flex";
@@ -168,194 +152,8 @@ closeM.addEventListener("click", () => {
 updateCurrentTime();
 setInterval(updateCurrentTime, 1000);
 
-// información del torneo
-dateM.value = `${day}/${month}/${year}`;
-
-inputs.forEach((input) => {
-  input.addEventListener("input", () => {
-    let allFilled = Array.from(inputs).every(
-      (input) => input.value.trim() !== ""
-    );
-    sendInfoBtnM.disabled = !allFilled;
-    resetInfoBtnM.disabled = false;
-    if (input.value.trim().length > 0) { // revisar!!!!!!!!!!!!!!!!!!
-      resetInfoBtnM.disabled = true;
-    }
-  });
-});
-
-sendInfoBtnM.addEventListener("click", () => {
-  title.textContent = nameM.value;
-  stack.textContent = parseInt(stackM.value).toLocaleString();
-  buyIn.textContent = parseInt(buyInM.value).toLocaleString();
-  endRebuys.textContent = " Nivel " + parseInt(endRebuysM.value);
-});
-
-resetInfoBtnM.addEventListener("click", () => {
-  title.textContent = "-";
-  stack.textContent = "-";
-  buyIn.textContent = "-";
-  endRebuys.textContent = " Nivel " + "-";
-  nameM.value = "";
-  stackM.value = "";
-  buyInM.value = "";
-  endRebuysM.value = "";
-  resetInfoBtnM.disabled = true;
-});
-
-// generar premios
-let arrayPrizes = [];
-
-paidPlaces.addEventListener("change", () => {
-  generatePrizes.disabled = true;
-
-  if (paidPlaces.value > 0) {
-    generatePrizes.disabled = false;
-  }
-});
-generatePrizes.addEventListener("click", () => {
-  let distribucion = [];
-
-  if (Number(paidPlaces.value) > 0) {
-    sendPrizesBtn.style.display = "block";
-    tbodyPrizes.textContent = "";
-
-    switch (Number(paidPlaces.value)) {
-      case 3:
-        distribucion = [
-          { place: "1°", percent: 50 },
-          { place: "2°", percent: 30 },
-          { place: "3°", percent: 20 },
-        ];
-        break;
-      case 4:
-        distribucion = [
-          { place: "1°", percent: 50 },
-          { place: "2°", percent: 25 },
-          { place: "3°", percent: 15 },
-          { place: "4°", percent: 10 },
-        ];
-        break;
-      case 5:
-        distribucion = [
-          { place: "1°", percent: 40 },
-          { place: "2°", percent: 25 },
-          { place: "3°", percent: 15 },
-          { place: "4°", percent: 10 },
-          { place: "5°", percent: 10 },
-        ];
-        break;
-      default:
-        alert("Número de jugadores no válido. Debe estar entre 3 y 5.");
-        return;
-    }
-
-    tablePrizes.style.display = "block";
-    arrayPrizes = [];
-
-    distribucion.forEach((item, i) => {
-      let tr = document.createElement("tr");
-
-      let tdPlace = document.createElement("td");
-      tdPlace.textContent = item.place;
-      tr.appendChild(tdPlace);
-
-      let tdPercent = document.createElement("td");
-      let inputPercent = document.createElement("input");
-      inputPercent.type = "number";
-      inputPercent.value = item.percent;
-      inputPercent.style.width = "50px";
-      inputPercent.addEventListener("input", (e) => {
-        let newPercent = Number(e.target.value);
-        let newPrize = (newPercent / 100) * Number(totalPrize.value);
-        inputPrize.value = newPrize;
-        arrayPrizes[i].percent = newPercent;
-        arrayPrizes[i].prize = newPrize;
-        updateTotals();
-      });
-      tdPercent.appendChild(inputPercent);
-      tr.appendChild(tdPercent);
-
-      let prize = Number(totalPrize.value) * (item.percent / 100);
-
-      let tdPrize = document.createElement("td");
-      let inputPrize = document.createElement("input");
-      inputPrize.type = "number";
-      inputPrize.value = prize;
-      inputPrize.style.width = "80px";
-      inputPrize.addEventListener("input", (e) => {
-        let newPrize = Number(e.target.value);
-        let newPercent = (newPrize / Number(totalPrize.value)) * 100;
-        inputPercent.value = newPercent;
-        arrayPrizes[i].percent = newPercent;
-        arrayPrizes[i].prize = newPrize;
-        updateTotals();
-      });
-      tdPrize.appendChild(inputPrize);
-      tr.appendChild(tdPrize);
-
-      tbodyPrizes.appendChild(tr);
-      arrayPrizes.push({
-        place: item.place,
-        percent: item.percent,
-        prize: prize,
-      });
-    });
-    updateTotals();
-  }
-});
-
-// enviar premios
-sendPrizesBtn.addEventListener("click", () => {
-  pPrizes.style.display = "flex";
-  pInfoMtt.style.display = "none";
-  pPrizes.innerHTML = "";
-
-  arrayPrizes.forEach((item) => {
-    pPrizes.innerHTML += `
-      ${item.place}: ${item.prize} € |
-    `;
-  });
-});
-
-// color tema
-theme.addEventListener("change", () => {
-  let selectedColor = theme.value;
-  let themeElements = [classOne, classTwo, classThree];
-  let color1 = "var(--blue1)";
-  let color2 = "var(--blue2)";
-  let color3 = "var(--blue3)";
-  let color4 = "var(--blue4)";
-
-  if (selectedColor == "red") {
-    color1 = "var(--red1)";
-    color2 = "var(--red2)";
-    color3 = "var(--red3)";
-    color4 = "var(--red4)";
-  } else if (selectedColor == "green") {
-    color1 = "var(--green1)";
-    color2 = "var(--green2)";
-    color3 = "var(--green3)";
-    color4 = "var(--green4)";
-  }
-
-  themeElements[0].forEach((element) => {
-    element.style.backgroundColor = color1;
-  });
-
-  themeElements[1].forEach((element) => {
-    element.style.backgroundColor = color2;
-  });
-
-  themeElements[2].forEach((element) => {
-    element.style.backgroundColor = color3;
-  });
-
-  document.body.style.backgroundColor = color4;
-});
-
-// control panel btn
-// entries
+// ***** panel de control *****
+// entrada -
 downEntry.addEventListener("click", () => {
   entries = Number(document.getElementById("sB").textContent);
   addons = Number(document.getElementById("sAd").textContent);
@@ -392,6 +190,8 @@ downEntry.addEventListener("click", () => {
     upAddon.disabled = true;
   }
 });
+
+// entrada +
 upEntry.addEventListener("click", () => {
   entries = Number(document.getElementById("sB").textContent);
   addons = Number(document.getElementById("sAd").textContent);
@@ -409,7 +209,7 @@ upEntry.addEventListener("click", () => {
   }
 });
 
-// rebuys
+// recompra -
 downRebuy.addEventListener("click", () => {
   rebuys = Number(document.getElementById("sR").textContent);
 
@@ -421,6 +221,8 @@ downRebuy.addEventListener("click", () => {
     downRebuy.disabled = true;
   }
 });
+
+// recompra +
 upRebuy.addEventListener("click", () => {
   rebuys = Number(document.getElementById("sR").textContent);
   rebuys += 1;
@@ -431,7 +233,7 @@ upRebuy.addEventListener("click", () => {
   }
 });
 
-// add ons
+// addon -
 downAddon.addEventListener("click", () => {
   entries = Number(document.getElementById("sB").textContent);
   addons = Number(document.getElementById("sAd").textContent);
@@ -452,6 +254,8 @@ downAddon.addEventListener("click", () => {
     upAddon.disabled = false;
   }
 });
+
+// addon +
 upAddon.addEventListener("click", () => {
   entries = Number(document.getElementById("sB").textContent);
   addons = Number(document.getElementById("sAd").textContent);
@@ -469,7 +273,7 @@ upAddon.addEventListener("click", () => {
   }
 });
 
-// players
+// jugador -
 downPlayer.addEventListener("click", () => {
   entries = Number(document.getElementById("sB").textContent);
   addons = Number(document.getElementById("sAd").textContent);
@@ -486,6 +290,8 @@ downPlayer.addEventListener("click", () => {
     upPlayer.disabled = false;
   }
 });
+
+// jugador +
 upPlayer.addEventListener("click", () => {
   entries = Number(document.getElementById("sB").textContent);
   addons = Number(document.getElementById("sAd").textContent);
@@ -501,4 +307,249 @@ upPlayer.addEventListener("click", () => {
   if (playersLeft != 0) {
     downPlayer.disabled = false;
   }
+});
+
+// //////////////////////////// MODAL ////////////////////////////
+// ******************** INFORMACION DEL TORNEO ********************
+dateM.value = `${day}/${month}/${year}`;
+
+inputs.forEach((input) => {
+  input.addEventListener("input", () => {
+    let allFilled = Array.from(inputs).every(
+      (input) => input.value.trim() !== ""
+    );
+    sendInfoBtnM.disabled = !allFilled;
+    resetInfoBtnM.disabled = false;
+  });
+});
+
+// click en enviar
+sendInfoBtnM.addEventListener("click", () => {
+  title.textContent = nameM.value;
+  stack.textContent = parseInt(stackM.value).toLocaleString();
+  buyIn.textContent = parseInt(buyInM.value).toLocaleString();
+  endRebuys.textContent = " Nivel " + parseInt(endRebuysM.value);
+  sendInfoBtnM.disabled = true;
+});
+
+// click en resetear
+resetInfoBtnM.addEventListener("click", () => {
+  title.textContent = "-";
+  stack.textContent = "-";
+  buyIn.textContent = "-";
+  endRebuys.textContent = " Nivel " + "-";
+  nameM.value = "";
+  stackM.value = "";
+  buyInM.value = "";
+  endRebuysM.value = "";
+  resetInfoBtnM.disabled = true;
+});
+
+// ******************** PREMIOS ********************
+// generar premios
+let arrayPrizes = [];
+
+paidPlaces.addEventListener("input", () => {
+  generatePrizesBtnM.disabled = true;
+  sendPrizesBtnM.style.display = "none";
+  tablePrizes.style.display = "none";
+
+  if (paidPlaces.value > 0 && Number(totalPrize.value) != 0) {
+    generatePrizesBtnM.disabled = false;
+  }
+});
+
+// click en generar
+generatePrizesBtnM.addEventListener("click", () => {
+  const distribucion = {
+    2: [
+      { place: "1°", percent: 60 },
+      { place: "2°", percent: 40 },
+    ],
+    3: [
+      { place: "1°", percent: 50 },
+      { place: "2°", percent: 30 },
+      { place: "3°", percent: 20 },
+    ],
+    4: [
+      { place: "1°", percent: 50 },
+      { place: "2°", percent: 25 },
+      { place: "3°", percent: 15 },
+      { place: "4°", percent: 10 },
+    ],
+    5: [
+      { place: "1°", percent: 40 },
+      { place: "2°", percent: 27 },
+      { place: "3°", percent: 16 },
+      { place: "4°", percent: 10 },
+      { place: "5°", percent: 7 },
+    ],
+    6: [
+      { place: "1°", percent: 40 },
+      { place: "2°", percent: 25 },
+      { place: "3°", percent: 15 },
+      { place: "4°", percent: 10 },
+      { place: "5°", percent: 6 },
+      { place: "6°", percent: 4 },
+    ],
+    7: [
+      { place: "1°", percent: 38 },
+      { place: "2°", percent: 24 },
+      { place: "3°", percent: 14 },
+      { place: "4°", percent: 10 },
+      { place: "5°", percent: 7 },
+      { place: "6°", percent: 5 },
+      { place: "7°", percent: 2 },
+    ],
+    8: [
+      { place: "1°", percent: 37 },
+      { place: "2°", percent: 21 },
+      { place: "3°", percent: 14 },
+      { place: "4°", percent: 9 },
+      { place: "5°", percent: 7 },
+      { place: "6°", percent: 5 },
+      { place: "7°", percent: 4 },
+      { place: "8°", percent: 3 },
+    ],
+    9: [
+      { place: "1°", percent: 35 },
+      { place: "2°", percent: 20 },
+      { place: "3°", percent: 14 },
+      { place: "4°", percent: 10 },
+      { place: "5°", percent: 7 },
+      { place: "6°", percent: 5 },
+      { place: "7°", percent: 4 },
+      { place: "8°", percent: 3 },
+      { place: "9°", percent: 2 },
+    ],
+    10: [
+      { place: "1°", percent: 33 },
+      { place: "2°", percent: 21 },
+      { place: "3°", percent: 12 },
+      { place: "4°", percent: 9 },
+      { place: "5°", percent: 7 },
+      { place: "6°", percent: 5 },
+      { place: "7°", percent: 4 },
+      { place: "8°", percent: 3.5 },
+      { place: "9°", percent: 3 },
+      { place: "10°", percent: 2.5 },
+    ],
+  };
+
+  const paidPlacesValue = Number(paidPlaces.value);
+
+  if (paidPlacesValue >= 2 && paidPlacesValue <= 10) {
+    sendPrizesBtnM.style.display = "block";
+    tbodyPrizes.textContent = "";
+    tablePrizes.style.display = "block";
+    arrayPrizes = [];
+
+    distribucion[paidPlacesValue].forEach((item, i) => {
+      let tr = document.createElement("tr");
+
+      let tdPlace = document.createElement("td");
+      tdPlace.textContent = item.place;
+      tr.appendChild(tdPlace);
+
+      let tdPercent = document.createElement("td");
+      let inputPercent = document.createElement("input");
+      inputPercent.type = "number";
+      inputPercent.value = item.percent;
+      inputPercent.style.width = "3.125rem";
+      inputPercent.style.height = "0.5rem";
+
+      inputPercent.addEventListener("input", (e) => {
+        let newPercent = Number(e.target.value);
+        let newPrize = (newPercent / 100) * Number(totalPrize.value);
+        inputPrize.value = newPrize.toFixed(2);
+        arrayPrizes[i].percent = newPercent;
+        arrayPrizes[i].prize = newPrize;
+        updateTotals();
+      });
+
+      tdPercent.appendChild(inputPercent);
+      tr.appendChild(tdPercent);
+
+      let prize = Number(totalPrize.value) * (item.percent / 100);
+      let tdPrize = document.createElement("td");
+      let inputPrize = document.createElement("input");
+      inputPrize.type = "number";
+      inputPrize.value = prize.toFixed(2);
+      inputPrize.style.width = "3.125rem";
+      inputPrize.style.height = "0.5rem";
+
+      inputPrize.addEventListener("input", (e) => {
+        let newPrize = Number(e.target.value);
+        let newPercent = (newPrize / Number(totalPrize.value)) * 100;
+        inputPercent.value = newPercent.toFixed(2);
+        arrayPrizes[i].percent = newPercent;
+        arrayPrizes[i].prize = newPrize;
+        updateTotals();
+      });
+
+      tdPrize.appendChild(inputPrize);
+      tr.appendChild(tdPrize);
+      tbodyPrizes.appendChild(tr);
+      arrayPrizes.push({
+        place: item.place,
+        percent: item.percent,
+        prize: prize,
+      });
+    });
+
+    updateTotals();
+  } else {
+    alert("Número de jugadores no válido. Debe estar entre 2 y 10 (incl.)");
+    paidPlaces.value = "";
+    generatePrizesBtnM.disabled = true;
+  }
+});
+// click en enviar
+sendPrizesBtnM.addEventListener("click", () => {
+  pPrizes.style.display = "flex";
+  pInfoMtt.style.display = "none";
+  pPrizes.innerHTML = "";
+
+  arrayPrizes.forEach((item) => {
+    pPrizes.innerHTML += `
+      ${item.place}: ${item.prize} € |
+    `;
+  });
+});
+
+// ******************** COLOR DEL TEMA ********************
+// select tema
+theme.addEventListener("change", () => {
+  let selectedColor = theme.value;
+  let themeElements = [classOne, classTwo, classThree];
+  let color1 = "var(--blue1)";
+  let color2 = "var(--blue2)";
+  let color3 = "var(--blue3)";
+  let color4 = "var(--blue4)";
+
+  if (selectedColor == "red") {
+    color1 = "var(--red1)";
+    color2 = "var(--red2)";
+    color3 = "var(--red3)";
+    color4 = "var(--red4)";
+  } else if (selectedColor == "green") {
+    color1 = "var(--green1)";
+    color2 = "var(--green2)";
+    color3 = "var(--green3)";
+    color4 = "var(--green4)";
+  }
+
+  themeElements[0].forEach((element) => {
+    element.style.backgroundColor = color1;
+  });
+
+  themeElements[1].forEach((element) => {
+    element.style.backgroundColor = color2;
+  });
+
+  themeElements[2].forEach((element) => {
+    element.style.backgroundColor = color3;
+  });
+
+  document.body.style.backgroundColor = color4;
 });
