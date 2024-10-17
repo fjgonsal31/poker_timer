38a40c72eeeeb2d979ba7f0890b5346ff6c90524
fjgonsal31 <fjgonsal31@gmail.com>
2024-10-17 14:49:03 +0200

    171024

--- public/js/js.js
+++ public/js/js.js
@@ -80,6 +80,531 @@ const tablePrizes = document.getElementById("tablePrizes");
 const tbodyPrizes = document.getElementById("tbodyPrizes");
 let totalRow = document.getElementById("totalRow");
 const theme = document.getElementById("themeColor");
+const selectLevels = document.getElementById("sectBlindsSelect");
+const tableBlindsLevels = document.getElementById("tableBlindsLevels");
+const tbodyLevels = document.getElementById("tbodyLevels");
+
+const levels = {
+  lento: {
+    name: "Lento",
+    levels: [
+      {
+        level: 1,
+        sb: 100,
+        bb: 100,
+        ante: 0,
+        time: 15,
+      },
+      {
+        level: 2,
+        sb: 100,
+        bb: 200,
+        ante: 0,
+        time: 15,
+      },
+      {
+        level: 3,
+        sb: 200,
+        bb: 300,
+        ante: 0,
+        time: 15,
+      },
+      {
+        level: 4,
+        sb: 200,
+        bb: 400,
+        ante: 0,
+        time: 15,
+      },
+      {
+        level: 5,
+        sb: 200,
+        bb: 500,
+        ante: 0,
+        time: 15,
+      },
+      {
+        level: 6,
+        sb: 300,
+        bb: 600,
+        ante: 0,
+        time: 15,
+      },
+      {
+        level: 7,
+        sb: 400,
+        bb: 800,
+        ante: 0,
+        time: 15,
+      },
+      {
+        level: 8,
+        sb: 500,
+        bb: 1000,
+        ante: 0,
+        time: 15,
+      },
+      {
+        level: 9,
+        sb: 500,
+        bb: 1000,
+        ante: 1000,
+        time: 15,
+      },
+      {
+        level: 10,
+        sb: 700,
+        bb: 1500,
+        ante: 1500,
+        time: 15,
+      },
+      {
+        level: 11,
+        sb: 1000,
+        bb: 2000,
+        ante: 2000,
+        time: 15,
+      },
+      {
+        level: 12,
+        sb: 1500,
+        bb: 3000,
+        ante: 3000,
+        time: 15,
+      },
+      {
+        level: 13,
+        sb: 2000,
+        bb: 4000,
+        ante: 4000,
+        time: 15,
+      },
+      {
+        level: 14,
+        sb: 2500,
+        bb: 5000,
+        ante: 5000,
+        time: 15,
+      },
+      {
+        level: 15,
+        sb: 3000,
+        bb: 6000,
+        ante: 6000,
+        time: 15,
+      },
+      {
+        level: 16,
+        sb: 4000,
+        bb: 8000,
+        ante: 8000,
+        time: 15,
+      },
+      {
+        level: 17,
+        sb: 6000,
+        bb: 12000,
+        ante: 12000,
+        time: 15,
+      },
+      {
+        level: 18,
+        sb: 8000,
+        bb: 16000,
+        ante: 16000,
+        time: 15,
+      },
+      {
+        level: 19,
+        sb: 10000,
+        bb: 20000,
+        ante: 20000,
+        time: 15,
+      },
+      {
+        level: 20,
+        sb: 15000,
+        bb: 30000,
+        ante: 30000,
+        time: 15,
+      },
+      {
+        level: 21,
+        sb: 20000,
+        bb: 40000,
+        ante: 40000,
+        time: 15,
+      },
+      {
+        level: 22,
+        sb: 25000,
+        bb: 50000,
+        ante: 50000,
+        time: 15,
+      },
+      {
+        level: 23,
+        sb: 30000,
+        bb: 60000,
+        ante: 60000,
+        time: 15,
+      },
+      {
+        level: 24,
+        sb: 40000,
+        bb: 80000,
+        ante: 80000,
+        time: 15,
+      },
+      {
+        level: 25,
+        sb: 50000,
+        bb: 100000,
+        ante: 100000,
+        time: 15,
+      },
+    ],
+  },
+  normal: {
+    name: "Normal",
+    levels: [
+      {
+        level: 1,
+        sb: 100,
+        bb: 200,
+        ante: 0,
+        time: 15,
+      },
+      {
+        level: 2,
+        sb: 200,
+        bb: 400,
+        ante: 0,
+        time: 15,
+      },
+      {
+        level: 3,
+        sb: 300,
+        bb: 600,
+        ante: 0,
+        time: 15,
+      },
+      {
+        level: 4,
+        sb: 400,
+        bb: 800,
+        ante: 0,
+        time: 15,
+      },
+      {
+        level: 5,
+        sb: 500,
+        bb: 1000,
+        ante: 0,
+        time: 15,
+      },
+      {
+        level: 6,
+        sb: 600,
+        bb: 1200,
+        ante: 0,
+        time: 15,
+      },
+      {
+        level: 7,
+        sb: 700,
+        bb: 1400,
+        ante: 0,
+        time: 15,
+      },
+      {
+        level: 8,
+        sb: 800,
+        bb: 1600,
+        ante: 0,
+        time: 15,
+      },
+      {
+        level: 9,
+        sb: 1000,
+        bb: 2000,
+        ante: 2000,
+        time: 15,
+      },
+      {
+        level: 10,
+        sb: 1200,
+        bb: 2500,
+        ante: 2500,
+        time: 15,
+      },
+      {
+        level: 11,
+        sb: 1500,
+        bb: 3000,
+        ante: 3000,
+        time: 15,
+      },
+      {
+        level: 12,
+        sb: 1700,
+        bb: 3500,
+        ante: 3500,
+        time: 15,
+      },
+      {
+        level: 13,
+        sb: 2000,
+        bb: 4000,
+        ante: 4000,
+        time: 15,
+      },
+      {
+        level: 14,
+        sb: 2500,
+        bb: 5000,
+        ante: 5000,
+        time: 15,
+      },
+      {
+        level: 15,
+        sb: 3000,
+        bb: 6000,
+        ante: 6000,
+        time: 15,
+      },
+      {
+        level: 16,
+        sb: 4000,
+        bb: 8000,
+        ante: 8000,
+        time: 15,
+      },
+      {
+        level: 17,
+        sb: 6000,
+        bb: 12000,
+        ante: 12000,
+        time: 15,
+      },
+      {
+        level: 18,
+        sb: 8000,
+        bb: 16000,
+        ante: 16000,
+        time: 15,
+      },
+      {
+        level: 19,
+        sb: 10000,
+        bb: 20000,
+        ante: 20000,
+        time: 15,
+      },
+      {
+        level: 20,
+        sb: 15000,
+        bb: 30000,
+        ante: 30000,
+        time: 15,
+      },
+      {
+        level: 21,
+        sb: 20000,
+        bb: 40000,
+        ante: 40000,
+        time: 15,
+      },
+      {
+        level: 22,
+        sb: 25000,
+        bb: 50000,
+        ante: 50000,
+        time: 15,
+      },
+      {
+        level: 23,
+        sb: 30000,
+        bb: 60000,
+        ante: 60000,
+        time: 15,
+      },
+      {
+        level: 24,
+        sb: 40000,
+        bb: 80000,
+        ante: 80000,
+        time: 15,
+      },
+      {
+        level: 25,
+        sb: 50000,
+        bb: 100000,
+        ante: 100000,
+        time: 15,
+      },
+    ],
+  },
+  rapido: {
+    name: "Rápido",
+    levels: [
+      {
+        level: 1,
+        sb: 100,
+        bb: 200,
+        ante: 100,
+        time: 15,
+      },
+      {
+        level: 2,
+        sb: 200,
+        bb: 400,
+        ante: 400,
+        time: 15,
+      },
+      {
+        level: 3,
+        sb: 300,
+        bb: 600,
+        ante: 600,
+        time: 15,
+      },
+      {
+        level: 4,
+        sb: 400,
+        bb: 800,
+        ante: 800,
+        time: 15,
+      },
+      {
+        level: 5,
+        sb: 500,
+        bb: 1000,
+        ante: 1000,
+        time: 15,
+      },
+      {
+        level: 6,
+        sb: 600,
+        bb: 1200,
+        ante: 1200,
+        time: 15,
+      },
+      {
+        level: 7,
+        sb: 800,
+        bb: 1600,
+        ante: 1600,
+        time: 15,
+      },
+      {
+        level: 8,
+        sb: 1000,
+        bb: 2000,
+        ante: 2000,
+        time: 15,
+      },
+      {
+        level: 9,
+        sb: 1500,
+        bb: 3000,
+        ante: 3000,
+        time: 15,
+      },
+      {
+        level: 10,
+        sb: 2000,
+        bb: 4000,
+        ante: 4000,
+        time: 15,
+      },
+      {
+        level: 11,
+        sb: 2500,
+        bb: 5000,
+        ante: 5000,
+        time: 15,
+      },
+      {
+        level: 12,
+        sb: 3000,
+        bb: 6000,
+        ante: 6000,
+        time: 15,
+      },
+      {
+        level: 13,
+        sb: 4000,
+        bb: 8000,
+        ante: 8000,
+        time: 15,
+      },
+      {
+        level: 14,
+        sb: 6000,
+        bb: 12000,
+        ante: 12000,
+        time: 15,
+      },
+      {
+        level: 15,
+        sb: 8000,
+        bb: 16000,
+        ante: 16000,
+        time: 15,
+      },
+      {
+        level: 16,
+        sb: 10000,
+        bb: 20000,
+        ante: 20000,
+        time: 15,
+      },
+      {
+        level: 17,
+        sb: 15000,
+        bb: 30000,
+        ante: 30000,
+        time: 15,
+      },
+      {
+        level: 18,
+        sb: 20000,
+        bb: 40000,
+        ante: 40000,
+        time: 15,
+      },
+      {
+        level: 19,
+        sb: 25000,
+        bb: 50000,
+        ante: 50000,
+        time: 15,
+      },
+      {
+        level: 20,
+        sb: 30000,
+        bb: 60000,
+        ante: 60000,
+        time: 15,
+      },
+      {
+        level: 21,
+        sb: 40000,
+        bb: 80000,
+        ante: 80000,
+        time: 15,
+      },
+      {
+        level: 22,
+        sb: 50000,
+        bb: 100000,
+        ante: 100000,
+        time: 15,
+      },
+    ],
+  },
+};
 
 //------------ funciones ------------
 //***** tiempos *****
@@ -351,7 +876,6 @@ let arrayPrizes = [];
 
 paidPlaces.addEventListener("input", () => {
   generatePrizesBtnM.disabled = true;
-  sendPrizesBtnM.style.display = "none";
   tablePrizes.style.display = "none";
 
   if (paidPlaces.value > 0 && Number(totalPrize.value) != 0) {
@@ -456,7 +980,7 @@ generatePrizesBtnM.addEventListener("click", () => {
       inputPercent.type = "number";
       inputPercent.value = item.percent;
       inputPercent.style.width = "3.125rem";
-      inputPercent.style.height = "0.5rem";
+      inputPercent.style.height = "0.7rem";
 
       inputPercent.addEventListener("input", (e) => {
         let newPercent = Number(e.target.value);
@@ -475,8 +999,8 @@ generatePrizesBtnM.addEventListener("click", () => {
       let inputPrize = document.createElement("input");
       inputPrize.type = "number";
       inputPrize.value = prize.toFixed(2);
-      inputPrize.style.width = "3.125rem";
-      inputPrize.style.height = "0.5rem";
+      inputPrize.style.width = "4.5rem";
+      inputPrize.style.height = "0.7rem";
 
       inputPrize.addEventListener("input", (e) => {
         let newPrize = Number(e.target.value);
@@ -496,7 +1020,7 @@ generatePrizesBtnM.addEventListener("click", () => {
         prize: prize,
       });
     });
-
+    generatePrizesBtnM.disabled = false;
     updateTotals();
   } else {
     alert("Número de jugadores no válido. Debe estar entre 2 y 10 (incl.)");
@@ -517,7 +1041,7 @@ sendPrizesBtnM.addEventListener("click", () => {
   });
 });
 
-// ******************** COLOR DEL TEMA ********************
+// ******************** TEMA ********************
 // select tema
 theme.addEventListener("change", () => {
   let selectedColor = theme.value;
@@ -552,4 +1076,72 @@ theme.addEventListener("change", () => {
   });
 
   document.body.style.backgroundColor = color4;
+});
+
+// ******************** NIVELES ********************
+Object.keys(levels).forEach((key) => {
+  let nameLevel = levels[key].name
+    .toLowerCase()
+    .normalize()
+    .replace(/[áàäâ]/g, "a")
+    .replace(/[éèëê]/g, "e")
+    .replace(/[íìïî]/g, "i")
+    .replace(/[úùüû]/g, "u");
+
+  selectLevels.innerHTML += `
+    <option value="${nameLevel}">${levels[key].name}</option>
+  `;
+});
+
+selectLevels.addEventListener("change", () => {
+  let selectedLevel = selectLevels.value
+    .toLowerCase()
+    .normalize()
+    .replace(/[áàäâ]/g, "a")
+    .replace(/[éèëê]/g, "e")
+    .replace(/[íìïî]/g, "i")
+    .replace(/[óòöô]/g, "o")
+    .replace(/[úùüû]/g, "u");
+  tbodyLevels.innerHTML = `
+      <tr>
+        <td><input type="number" class="inputLevel" min="1" step="1"></td>
+        <td><input type="number" class="inputLevel" min="100" step="100"></td>
+        <td><input type="number" class="inputLevel" min="100" step="100"></td>
+        <td><input type="number" class="inputLevel" min="100" step="100"></td>
+        <td><input type="number" class="inputLevel" step="1" min="10"></td>
+      <tr>
+    `;
+  if (levels[selectedLevel]) {
+    const nameLevel = levels[selectedLevel];
+    let total = 0;
+    let totalH = 0;
+    let totalM = 0;
+    tbodyLevels.innerHTML = "";
+
+    nameLevel.levels.forEach((level, index) => {
+      total += level.time;
+      totalH = parseInt(total / 60);
+      totalM = (total / 60 - totalH) * 60;
+      tbodyLevels.innerHTML += `
+          <tr>
+            <td>${level.level}</td>
+            <td>${level.sb}</td>
+            <td>${level.bb}</td>
+            <td>${level.ante}</td>
+            <td>${level.time}</td>
+          </tr>
+          `;
+    });
+    tbodyLevels.innerHTML += `
+    <tr>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td>${totalH}:${totalM} h</td>
+    </tr>
+    `;
+  } else {
+    console.log("El torneo seleccionado no existe.");
+  }
 });
